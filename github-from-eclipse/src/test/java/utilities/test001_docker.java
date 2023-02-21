46eb26cf0165235291683e4bcfbc0c371bdffbed
sundhareswar <sundharmathi26@gmail.com>
2023-02-21 16:18:58 +0530

    Commited after correction

--- github-from-eclipse/src/test/java/utilities/test001_docker.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class test001_docker {
-public static void main(String[] args) {
-	System.out.println("Hello sundhar"+"First change for jenkins");
-}
-}
---
+++ src/test/java/util/Test001.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Test001 {
+
+	public static void test_new_001() {
+		System.out.println("Hello Sundhar"+"For jenkins testing");
+
+	}
+
+public static void main(String[] args) {
+	Test001 tc= new Test001();
+	tc.test_new_001();
+}
+}
